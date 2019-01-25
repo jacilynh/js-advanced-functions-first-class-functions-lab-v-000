69b1829f1d412b0931b5dc731b04deb6510f7710
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-25 05:14:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Declare a variable with `const` that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the **first** two drivers in the array.
 const returnFirstTwoDrivers = function(drivers) {
-  return drivers.splice(0, 2);
+  return drivers.slice(0, 2);
 };
 
 // Declare a variable with `const` that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the **last** two drivers in the array.
