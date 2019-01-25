04d534fd6e6f0b8cdda0d4fbcfd95eb27daa8f09
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-25 05:44:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 function createFareMultiplier(int) {
   return function(fare) {
     return fare * int;
-  }
-}
+  };
+};
 
 // Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()` function accepts a fare as its lone argument and doubles it.
 const fareDoubler = createFareMultiplier(2)
